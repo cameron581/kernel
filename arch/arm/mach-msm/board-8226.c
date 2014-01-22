dc386fbf9fd50f99e8c2a48a405b5fdb8cb67eaf
cameron581 <cameron581@cox.net>
2014-01-22 15:28:45 -0600

    Update board-8226.c

--- arch/arm/mach-msm/board-8226.c
+++ arch/arm/mach-msm/board-8226.c
@@ -54,55 +54,53 @@
 #include "modem_notifier.h"
 
 static struct memtype_reserve msm8226_reserve_table[] __initdata = {
-	[MEMTYPE_SMI] = {
-	},
-	[MEMTYPE_EBI0] = {
-		.flags	=	MEMTYPE_FLAGS_1M_ALIGN,
-	},
-	[MEMTYPE_EBI1] = {
-		.flags	=	MEMTYPE_FLAGS_1M_ALIGN,
-	},
+        [MEMTYPE_SMI] = {
+        },
+        [MEMTYPE_EBI0] = {
+                .flags        =        MEMTYPE_FLAGS_1M_ALIGN,
+        },
+        [MEMTYPE_EBI1] = {
+                .flags        =        MEMTYPE_FLAGS_1M_ALIGN,
+        },
 };
 
 static int msm8226_paddr_to_memtype(unsigned int paddr)
 {
-	return MEMTYPE_EBI1;
+        return MEMTYPE_EBI1;
 }
 
 static struct of_dev_auxdata msm8226_auxdata_lookup[] __initdata = {
-	OF_DEV_AUXDATA("qcom,msm-sdcc", 0xF9824000, \
-			"msm_sdcc.1", NULL),
-	OF_DEV_AUXDATA("qcom,msm-sdcc", 0xF98A4000, \
-			"msm_sdcc.2", NULL),
-	OF_DEV_AUXDATA("qcom,msm-sdcc", 0xF9864000, \
-			"msm_sdcc.3", NULL),
-	OF_DEV_AUXDATA("qcom,sdhci-msm", 0xF9824900, \
-			"msm_sdcc.1", NULL),
-	OF_DEV_AUXDATA("qcom,sdhci-msm", 0xF98A4900, \
-			"msm_sdcc.2", NULL),
-	OF_DEV_AUXDATA("qcom,sdhci-msm", 0xF9864900, \
-			"msm_sdcc.3", NULL),
-	OF_DEV_AUXDATA("qcom,hsic-host", 0xF9A00000, "msm_hsic_host", NULL),
-
-	{}
+        OF_DEV_AUXDATA("qcom,msm-sdcc", 0xF9824000, \
+                        "msm_sdcc.1", NULL),
+        OF_DEV_AUXDATA("qcom,msm-sdcc", 0xF98A4000, \
+                        "msm_sdcc.2", NULL),
+        OF_DEV_AUXDATA("qcom,msm-sdcc", 0xF9864000, \
+                        "msm_sdcc.3", NULL),
+        OF_DEV_AUXDATA("qcom,sdhci-msm", 0xF9824900, \
+                        "msm_sdcc.1", NULL),
+        OF_DEV_AUXDATA("qcom,sdhci-msm", 0xF98A4900, \
+                        "msm_sdcc.2", NULL),
+        OF_DEV_AUXDATA("qcom,sdhci-msm", 0xF9864900, \
+                        "msm_sdcc.3", NULL),
+        {}
 };
 
 static struct reserve_info msm8226_reserve_info __initdata = {
-	.memtype_reserve_table = msm8226_reserve_table,
-	.paddr_to_memtype = msm8226_paddr_to_memtype,
+        .memtype_reserve_table = msm8226_reserve_table,
+        .paddr_to_memtype = msm8226_paddr_to_memtype,
 };
 
 static void __init msm8226_early_memory(void)
 {
-	reserve_info = &msm8226_reserve_info;
-	of_scan_flat_dt(dt_scan_for_memory_hole, msm8226_reserve_table);
+        reserve_info = &msm8226_reserve_info;
+        of_scan_flat_dt(dt_scan_for_memory_hole, msm8226_reserve_table);
 }
 
 static void __init msm8226_reserve(void)
 {
-	reserve_info = &msm8226_reserve_info;
-	of_scan_flat_dt(dt_scan_for_memory_reserve, msm8226_reserve_table);
-	msm_reserve();
+        reserve_info = &msm8226_reserve_info;
+        of_scan_flat_dt(dt_scan_for_memory_reserve, msm8226_reserve_table);
+        msm_reserve();
 }
 
 /*
@@ -113,56 +111,56 @@ static void __init msm8226_reserve(void)
  */
 void __init msm8226_add_drivers(void)
 {
-	msm_smem_init();
-	msm_init_modem_notifier_list();
-	msm_smd_init();
-	msm_rpm_driver_init();
-	msm_spm_device_init();
-	msm_pm_sleep_status_init();
-	rpm_regulator_smd_driver_init();
-	qpnp_regulator_init();
-	if (of_board_is_rumi())
-		msm_clock_init(&msm8226_rumi_clock_init_data);
-	else
-		msm_clock_init(&msm8226_clock_init_data);
-	tsens_tm_init_driver();
+        msm_smem_init();
+        msm_init_modem_notifier_list();
+        msm_smd_init();
+        msm_rpm_driver_init();
+        msm_spm_device_init();
+        msm_pm_sleep_status_init();
+        rpm_regulator_smd_driver_init();
+        qpnp_regulator_init();
+        if (of_board_is_rumi())
+                msm_clock_init(&msm8226_rumi_clock_init_data);
+        else
+                msm_clock_init(&msm8226_clock_init_data);
+        tsens_tm_init_driver();
 #ifdef CONFIG_INTELLI_THERMAL
-	msm_thermal_init(NULL);
+        msm_thermal_init(NULL);
 #else
-	msm_thermal_device_init(NULL);
+        msm_thermal_device_init(NULL);
 #endif
 }
 
 void __init msm8226_init(void)
 {
-	struct of_dev_auxdata *adata = msm8226_auxdata_lookup;
+        struct of_dev_auxdata *adata = msm8226_auxdata_lookup;
 
-	if (socinfo_init() < 0)
-		pr_err("%s: socinfo_init() failed\n", __func__);
+        if (socinfo_init() < 0)
+                pr_err("%s: socinfo_init() failed\n", __func__);
 
-	/* Don't do hard-coded muxing for moto platform */
-	if (!platform_is_msm8226_moto())
-		msm8226_init_gpiomux();
-	board_dt_populate(adata);
-	msm8226_add_drivers();
+        /* Don't do hard-coded muxing for moto platform */
+        if (!platform_is_msm8226_moto())
+                msm8226_init_gpiomux();
+        board_dt_populate(adata);
+        msm8226_add_drivers();
 }
 
 static const char *msm8226_dt_match[] __initconst = {
-	"qcom,msm8226",
-	"qcom,msm8926",
-	"qcom,apq8026",
-	NULL
+        "qcom,msm8226",
+        "qcom,msm8926",
+        "qcom,apq8026",
+        NULL
 };
 
 DT_MACHINE_START(MSM8226_DT, "Qualcomm MSM 8226 (Flattened Device Tree)")
-	.map_io = msm_map_msm8226_io,
-	.init_irq = msm_dt_init_irq,
-	.init_machine = msm8226_init,
-	.handle_irq = gic_handle_irq,
-	.timer = &msm_dt_timer,
-	.dt_compat = msm8226_dt_match,
-	.reserve = msm8226_reserve,
-	.init_very_early = msm8226_early_memory,
-	.restart = msm_restart,
-	.smp = &arm_smp_ops,
+        .map_io = msm_map_msm8226_io,
+        .init_irq = msm_dt_init_irq,
+        .init_machine = msm8226_init,
+        .handle_irq = gic_handle_irq,
+        .timer = &msm_dt_timer,
+        .dt_compat = msm8226_dt_match,
+        .reserve = msm8226_reserve,
+        .init_very_early = msm8226_early_memory,
+        .restart = msm_restart,
+        .smp = &arm_smp_ops,
 MACHINE_END
